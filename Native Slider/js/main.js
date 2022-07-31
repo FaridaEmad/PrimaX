a80892f9dc8d28cff764ceea9326ab27070136d2
fatmaashraf1 <101283196+FaridaEmad@users.noreply.github.com>
2022-07-31 11:35:59 +0200

    Native Slider

---
+++ Native Slider/js/main.js
@@ -0,0 +1,28 @@
+document.querySelectorAll(".carousel").forEach(carousel => {
+    const items = carousel.querySelectorAll(".carousel-item");
+    const but = Array.from(items, () => {
+        return `<span class="carousel-button"></span>`;
+    });
+
+    carousel.insertAdjacentHTML("beforeend",`
+    <div class="carousel-nav">
+        ${but.join("")} 
+    </div>`);
+
+    const buttons = carousel.querySelectorAll(".carousel-button");
+
+    buttons.forEach((button, i) => {
+        button.addEventListener("click", () =>{
+            //unselect all the items 
+            items.forEach(item => item.classList.remove("carousel-item-selected"));
+            buttons.forEach(button => button.classList.remove("carousel-button-selected"));
+
+            items[i].classList.add("carousel-item-selected");
+            button.classList.add("carousel-button-selected");
+        });
+    });
+
+    // select the first item on page load
+    items[0].classList.add("carousel-item-selected");
+    buttons[0].classList.add("carousel-button-selected");
+});
