4f94325c9a3cc5ea62585c9f7aa1327b425ee231
fatmaashraf1 <101283196+FaridaEmad@users.noreply.github.com>
2022-08-01 12:05:34 +0200

    updates

--- SmartSpace/javascript/script.js
+++ SmartSpace/javascript/script.js
@@ -6,3 +6,15 @@ window.onscroll = () => {
         navbar.classList.remove('nav-active');
     }
 };
+
+const propreties = document.querySelectorAll('.property');
+propreties.forEach((proprety ,i) => {
+    proprety.addEventListener("click", () => {
+        //unselect all the items
+        propreties.forEach(proprety => proprety.classList.remove('prop-active'));
+
+        propreties[i].classList.add("prop-active");
+    });
+    // select the first item on page load
+    propreties[0].classList.add("prop-active");
+});
